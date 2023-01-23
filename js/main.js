76d69c30c9170596d3e8f01125f16835bd16e78e
Mahmoud <mahmoudd188@gmail.com>
2023-01-23 23:41:43 +0200

    calc

---
+++ js/main.js
@@ -0,0 +1,153 @@
+// var num1 = Number(window.prompt("Enter first number"));
+// var num2 = Number(window.prompt("Enter secound number"));
+// var result = num1 + num2;
+// document.getElementById('demo').innerHTML= result;
+
+// var userRole =window.prompt('Enter your Role');
+// if( userRole == "admin"  ){
+//     document.getElementById('demo').innerHTML = 'you can make every thing';
+// }
+// else if( userRole == 'user' ) {
+//     document.getElementById('demo').innerHTML = 'you can see us only';
+// }
+// else if ( uerRole == 'moderator'  )
+// {
+//     document.getElementById('demo').innerHTML = 'you can not do any thing '; 
+// }
+// else{
+//     document.getElementById('demo').innerHTML = 'hello get out ';
+// // }
+// var username = window.prompt('Enter your name ');
+// if (username == 'Ahmed'){
+//     document.getElementById('demo').innerHTML= "hello"+ username;
+// }else if (username == 'Mohamed'){
+//     document.getElementById('demo').innerHTML= "hello"+ username;
+// }else if (username == 'Ayman'){
+//     document.getElementById('demo').innerHTML= "hello"+ username;
+// }else{
+//     document.getElementsByClassName('anim').innerHTML= "please Enter your name";
+// 
+// var firstName = window.prompt('Enter your first name');
+// var lastName = window.prompt('Enter your age ');
+
+// document.getElementById('demo').innerHTML = firstName;
+// document.getElementById('antimo').innerHTML = lastName;
+
+
+// var userName = window.prompt("Enter youn name");
+// var userAge = window.prompt("Enter your age");
+// var userData = userName + userAge ;
+// // document.getElementById('demo').innerHTML = userData;
+// var userNumber = Number(window.prompt('Enter your number'));
+
+// if (userNumber % 2 == 0){
+//     console.log("even");
+// }else{
+//     console.log('odd');
+// }
+
+                                 // sestion js ahmed bahnasy 22/1/2023
+
+
+
+//  function calcProduct (price, ads , profit , taxes)
+//  {
+//      var price1 = price + ads ;
+//      var price2 = price1 - (price1 * taxes );
+//      var price3 = price2 + profit;
+//      console.log(price3);
+//  }
+//  calcProduct(100,20, 50, 0.2);
+// function getAvg (x , y)
+// {
+//     var sum = x+ y;
+//     var result = sum/2;
+//     return result;
+// }
+//  var x  = getAvg (10, 20);
+// alert(x);
+
+// function addElements( elementNumber,elementId,element)
+// {
+//     var cartona ="";
+//     for( var i= 0;i<elementNumber ;i++){
+//         cartona+=element;
+//     }
+//     document.getElementById(elementId).innerHTML =cartona;
+// }
+// addElements(20,"profile", "<h3> web design</h3>")
+// addElements(10,"services", "<p > web design</p>")
+// addElements(8,"demo", "<img src='img/drew-coffman-1872.jpg'></img>")
+                            // end sesstion sunday
+
+    // function calcAvg ( x , y)
+    // {
+    //     var sum = x + y;
+    //     var result = sum/2;
+    //     console.log(result);
+    // }
+    // calcAvg(10,5); //cal invoke 
+// var userYear = Number(window.prompt('enter year '));
+// var userMonth = Number(window.prompt('enter month '));
+// var userDay = Number(window.prompt('enter day '));
+//     function calcAge (y , m, d)
+//     {
+//         y -=  2023;
+//         m -= 1;
+//         d -= 22;
+//         console.log(d + "/" + m +"/" + y);
+//         return calcAge();
+//     }
+//     calcAge(userYear,userMonth,userDay);
+
+
+// calc product price 
+
+//  function addCard (elementId, elementNumber, element)
+//  {
+//      var cartona ="";
+//      for (i=0; i<elementNumber;i++){
+//         cartona += element;
+//      }
+//      document.getElementById(elementId).innerHTML = cartona;
+//  }
+
+//  addCard ('profile', 20, '<p>hi</p>')
+//  addCard ('services', 10, '<div>iam amaf</div>')
+//  addCard ('demo', 12, '<img src="../img/someone write on computer.jpeg">')
+
+
+// var x = window.prompt('enter your name');
+// var y = window.prompt('enter your age');
+// var z= window.prompt('enter your phone');
+// function addperson (userName = "---", userAge =0 , userPhoe=0)
+// {
+
+//     if (userName.length<= 6)   //check name
+//     {
+//         alert ('you should enter full name');
+//     }else{
+//          document.getElementById('person').innerHTML = 
+//     userName + " " +  userAge +"" +  userPhoe ;
+//     if ( userAge <=18){        //check age 
+//         alert ('your age is not allow');
+//     }else{
+//         document.getElementById('person').innerHTML = 
+//         userName + " " +  userAge +"" +  userPhoe ;
+//     }
+//      if ( userPhoe.length< 11){ //check phone number
+//         alert('invaild number');
+//     }else if (userPhoe > 11){
+//         alert ('this is not a number phone')
+//     }
+//     else{
+//         document.getElementById('person').innerHTML = 
+//         userName + " " +  userAge +"" +  userPhoe ;
+//     }
+//     }
+// }
+// addperson(x,y,z);
+
+
+
+
